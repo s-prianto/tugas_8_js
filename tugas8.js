80206075c3b1e56a31efad7a2bb85b6079308d29
s-prianto <prianto2796@gmail.com>
2020-02-12 06:21:31 +0700

    Tugas 8 JS

---
+++ tugas8.js
@@ -0,0 +1,15 @@
+function objectbahasapemograman(){
+  var belajarpemograman = {
+    nama_bootcamp : "NIOMIC",
+    bahasa_pemograman : ["html","css","javascript"],
+    tahun : 2019
+  }
+  belajarpemograman.tahun=2020;
+  console.log(belajarpemograman);
+  console.log(belajarpemograman.nama_bootcamp);
+  console.log(belajarpemograman.bahasa_pemograman);
+  console.log(belajarpemograman.bahasa_pemograman[1]);
+  console.log(belajarpemograman.tahun);
+}
+
+objectbahasapemograman()
